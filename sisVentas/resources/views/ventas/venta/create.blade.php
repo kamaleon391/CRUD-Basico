cff497705da8800ac3faf5f9e752a00711d87a54
kamaleon391 <kamaleon391@gmail.com>
2018-06-28 23:33:33 -0500

    Registro ventas almacen

--- sisVentas/resources/views/ventas/venta/create.blade.php
+++ sisVentas/resources/views/ventas/venta/create.blade.php
@@ -2,7 +2,7 @@
 @section ('contenido')
 	<div class="row">
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-			<h3>Nuevo Ingreso</h3>
+			<h3>Nueva Venta</h3>
 			@if (count($errors) > 0)
 				<div class="alert alert-danger">
 					<ul>
@@ -14,14 +14,14 @@
 			@endif
 		</div>
 	</div>
-	{!!Form::open(array('url'=>'compras/ingreso','method'=>'POST','autocomplete'=>'off'))!!}
+	{!!Form::open(array('url'=>'ventas/venta','method'=>'POST','autocomplete'=>'off'))!!}
 	{{ Form::token() }}
 
 		<div class="row">
 			<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
 				<div class="form-group">
-					<label for="proveedor">Proveedor</label>
-					<select name="idproveedor" id="idproveedor" class="form-control selectpicker" data-live-search="true">
+					<label for="cliente">Cliente</label>
+					<select name="idcliente" id="idcliente" class="form-control selectpicker" data-live-search="true">
 						@foreach($personas as $persona)
 							<option value="{{$persona->idpersona}}">{{$persona->nombre}}</option>
 						@endforeach
@@ -60,7 +60,7 @@
 							<label>Artículo</label>
 							<select name="pidarticulo" class="form-control selectpicker" id="pidarticulo" data-live-search="true">
 								@foreach($articulos as $articulo)
-									<option value="{{$articulo->idarticulo}}">{{$articulo->articulo}}</option>
+									<option value="{{$articulo->idarticulo}}">{{$articulo->articulo}}_{{$articulo->stock}}_{{$articulo->precio_promedio}}</option>
 								@endforeach
 							</select>
 						</div>
@@ -68,19 +68,25 @@
 					<div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
 						<div class="form-group">
 							<label for="cantidad">Cantidad</label>
-							<input type="number" name="pcantidad" id="pcantidad" class="form-control" placeholder="cantidad">
+							<input type="number" name="pcantidad" id="pcantidad" class="form-control" placeholder="Cantidad">
 						</div>
 					</div>
 					<div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
 						<div class="form-group">
-							<label for="precio_compra">Precio Compra</label>
-							<input type="number" name="pprecio_compra" id="pprecio_compra" class="form-control" placeholder="P. Compra">
+							<label for="stock">Existencia</label>
+							<input type="number" disabled name="pstock" id="pstock" class="form-control" placeholder="Existencia">
 						</div>
 					</div>
 					<div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
 						<div class="form-group">
 							<label for="precio_venta">Precio Venta</label>
-							<input type="number" name="pprecio_venta" id="pprecio_venta" class="form-control" placeholder="P. Venta">
+							<input type="number" disabled name="pprecio_venta" id="pprecio_venta" class="form-control" placeholder="P. Venta">
+						</div>
+					</div>
+					<div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
+						<div class="form-group">
+							<label for="descuento">Descuento</label>
+							<input type="number" name="pdescuento" id="pdescuento" class="form-control" placeholder="Descuento">
 						</div>
 					</div>
 					<div class="col-lg-2 col-sm-2 col-md-2 col-xs-12">
@@ -95,8 +101,8 @@
 								<th>Opciones</th>
 								<th>Artículo</th>
 								<th>Cantidad</th>
-								<th>Precio Compra</th>
 								<th>Precio Venta</th>
+								<th>Descuento</th>
 								<th>Subtotal</th>
 							</thead>
 							<tfoot>
@@ -105,7 +111,7 @@
 									<th></th>
 									<th></th>
 									<th></th>
-									<th><h4 id="total">$ 0.00</h4></th>
+									<th><h4 id="total">$ 0.00</h4><input type="hidden" name="total_venta" id="total_venta"></th>
 							</tfoot>
 							<tbody>
 							</tbody>
@@ -138,6 +144,14 @@
 		total = 0;
 		subtotal =[];
 		$("#guardar").hide();
+		$("#pidarticulo").change(mostrarValores);
+
+		function mostrarValores()
+		{
+			datosArticulo=document.getElementById('pidarticulo').value.split('_');
+			$("#pprecio_venta").val(datosArticulo[2]);
+			$("#pstock").val(datosArticulo[1]);
+		}
 
 		function agregar()
 		{
